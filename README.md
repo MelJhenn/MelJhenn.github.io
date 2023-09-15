9eea977b5b552e19735c90afe56e648e18295f40
MelJhenn <78235574+MelJhenn@users.noreply.github.com>
2023-09-15 17:48:13 -0400

    cambiando

---
+++ README.md
@@ -0,0 +1,2 @@
+# Catalogo-digital-ITIBB
+ Catalogo digital de los proyectos de titulación del ITIBB
---
+++ js/footer.js
@@ -0,0 +1,26 @@
+$(function () {
+    $("#mifooter").addClass("u-align-center u-clearfix u-footer u-grey-80 u-footer").html(`
+    <div class="about">
+      <div>
+         <p class="title-about">Acerca de Nosotros</p>
+         <p>
+            Instituto Tecnológico Industrial Brasil Bolivia, al servicio de la población Boliviana. <br>
+            © Copyright 2023 - Todos los derechos reservados.
+         </p>
+      </div>
+      <div>
+         <ul class="socials-wrapper">
+            <li><a href="https://www.facebook.com/InstitutoTecnologicoIndustrialBrasilBolivia" target="_blank"><img src="../img/facebook.svg" alt="facebook logo" draggable="false"></a></li>
+            <li><a href="https://www.instagram.com" target="_blank"><img src="../img/instagram.svg" alt="instagram logo" draggable="false"></a></li>
+         </ul>
+      </div>
+    </div>
+          `);
+});
+
+
+
+
+
+
+
