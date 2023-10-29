8b0f886098b726f171b1134c20ffb6e126933ade
MelJhenn <78235574+MelJhenn@users.noreply.github.com>
2023-10-29 10:34:48 -0400

    menu carreras

--- js/menu.js
+++ js/menu.js
@@ -5,30 +5,32 @@ $(function () {
         </li>
         <li class="u-nav-item">Carreras</li>
         </ul>
-        <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2">
+        <ul class="u-nav u-popupmenu-items u-unstyled u-nav-2 u-padding-left">
         <li class="u-nav-item"><a class="u-button-style u-nav-link active"
-        href="emd.html" title="Electromedicina">EMD</a>
+        href="emd.html" title="Electromedicina">Electromedicina</a>
         </li>
         <li class="u-nav-item"><a class="u-button-style u-nav-link active"
-        href="ccc.html" title="Construcción Civil">CCC</a>
+        href="ccc.html" title="Construcción Civil">Construcción Civil</a>
         </li>
         <li class="u-nav-item"><a class="u-button-style u-nav-link active"
-        href="emc.html" title="Electromecánica">EMC</a>
+        href="emc.html" title="Electromecánica">Electromecánica</a>
         </li>
         <li class="u-nav-item"><a class="u-button-style u-nav-link active"
-        href="iin.html" title="Informática Industrial">IIN</a>
+        href="iin.html" title="Informática Industrial">Informática Industrial</a>
         </li>
         <li class="u-nav-item"><a class="u-button-style u-nav-link active"
-        href="gpp.html" title="Gas, Petróleo y Procesos">GPP</a>
+        href="gpp.html" title="Gas, Petróleo y Procesos">Gas, Petróleo y Procesos</a>
         </li>
         <li class="u-nav-item"><a class="u-button-style u-nav-link active"
-        href="itc.html" title="Industria Textil y Confección">ITC</a>
+        href="itc.html" title="Industria Textil y Confección">Industria Textil y Confección</a>
         </li>
         <li class="u-nav-item"><a class="u-button-style u-nav-link active"
-        href="msf.html" title="Metalurgia, Siderurgia y Fundición">MSF</a>
+        href="msf.html" title="Metalurgia, Siderurgia y Fundición">Metalurgia, Siderurgia y Fundición</a>
         </li>
+        </ul>
+        <ul class="u-nav u-popupmenu-items u-unstyled u-nav-2">
         <li class="u-nav-item"><a class="u-button-style u-nav-link active"
-        href="http://itibb.edu.bo/">Contactanos</a>
+        href="http://itibb.edu.bo/">Contáctanos</a>
         </li>
         </ul>
         `);
