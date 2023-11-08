40e13fd0e7002fd88b37a8a6cc431d64e4a330af
MELDORYS <148693863+MELDORYS@users.noreply.github.com>
2023-11-08 17:18:23 -0400

    generado automaticamente

--- convertir.py
+++ convertir.py
@@ -1,10 +1,16 @@
 #to support encodings
+import os
 import codecs
 path='./json/base.json'
+path2='./json/base2.json'
 #read input file
-with codecs.open(path, 'r', encoding = 'utf8') as file:
-  lines = file.read()
-
-#write output file
-with codecs.open(path, 'w', encoding = 'utf8') as file:
-  file.write(lines)
+blockSize = 1048576
+with codecs.open(path,"r",encoding="mbcs") as sourceFile:
+    with codecs.open(path2,"w",encoding="UTF-8") as targetFile:
+        while True:
+            contents = sourceFile.read(blockSize)
+            if not contents:
+                break
+            targetFile.write(contents)
+os.remove(path)
+os.rename(path2, path)
