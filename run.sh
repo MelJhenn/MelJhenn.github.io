62acb72ee546b80a2cb5d6f7d01f07e35cbf5cb2
MELDORYS <148693863+MELDORYS@users.noreply.github.com>
2023-11-08 17:29:30 -0400

    generado automaticamente

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 cd C:/Users/lenovo/Documents/GitHub/MelJhenn.github.io
-C:\Users\lenovo\AppData\Local\Programs\Python\Python312\python.exe convertir.py
+C:/Users/lenovo/AppData/Local/Programs/Python/Python312/python.exe convertir.py
 git add -A
 git commit -m "generado automaticamente"
 git push
